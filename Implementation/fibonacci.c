b88298a67436054d14e3bfb6b092d90e9f259ac9
skakked <skakked@gmail.com>
2025-10-15 00:19:26 -0400

    > Added Folders > Started Implementing the Series in C

---
+++ Implementation/fibonacci.c
@@ -0,0 +1,11 @@
+/*
+ * Fibonacci Series Implementation in C
+ * Name: Siddharth Kakked
+ * Date: 14th October 2025
+ * Implements three algorithms for computing Fibonacci numbers:
+ * 1. Iterative 
+ * 2. Recursive 
+ * 3. Dynamic Programming 
+ * 
+ */
+
